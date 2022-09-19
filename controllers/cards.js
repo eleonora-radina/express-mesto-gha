09fc2399181271eeade4a1bbc907c739a7b21048
Eleonora Nikerina <eleonora.radina@mail.ru>
2022-09-20 00:34:34 +0400

    бекенд место

---
+++ controllers/cards.js
@@ -0,0 +1,85 @@
+const Card = require('../models/card');
+
+const getCards = async (req, res) => {
+  try {
+    const cards = await Card.find({});
+    res.status(200).send(cards);
+  } catch (e) {
+    res.status(500).send({ message: 'Ошибка по умолчанию.' });
+  }
+};
+
+const createCard = async (req, res) => {
+  try {
+    const owner = req.user._id;
+    const { name, link } = req.body;
+    const card = await Card.create({ name, link, owner });
+    res.status(201).send(card);
+  } catch (e) {
+    if (e.name === 'ValidationError') {
+      res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
+    } else {
+      res.status(500).send({ message: 'Ошибка по умолчанию.' });
+    }
+  }
+};
+
+const deleteCard = async (req, res) => {
+  try {
+    const { cardId } = req.params;
+    const card = await Card.findById(cardId);
+    if (!card) {
+      return res.status(404).send({ message: 'Карточка с указанным _id не найдена.' });
+    }
+    const cardDelete = await Card.findByIdAndRemove(cardId);
+    return res.status(200).send(cardDelete);
+  } catch (e) {
+    return res.status(500).send({ message: 'Ошибка по умолчанию.' });
+  }
+};
+
+const likeCard = async (req, res) => {
+  try {
+    const { cardId } = req.params;
+    const card = await Card.findById(cardId);
+    if (!card) {
+      return res.status(404).send({ message: 'Карточка с указанным _id не найдена.' });
+    }
+    const cardL = await Card.findByIdAndUpdate(
+      cardId,
+      { $addToSet: { likes: req.user._id } },
+      { new: true },
+    );
+    return res.status(200).send(cardL);
+  } catch (e) {
+    if (e.name === 'ValidationError') {
+      return res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
+    } return res.status(500).send({ message: 'Ошибка по умолчанию.' });
+  }
+};
+
+const dislikeCard = async (req, res) => {
+  try {
+    const card = await Card.findByIdAndUpdate(
+      req.params.cardId,
+      { $pull: { likes: req.user._id } },
+      { new: true },
+    );
+    if (!card) {
+      return res.status(404).send({ message: 'Карточка с указанным _id не найдена.' });
+    }
+    return res.status(200).send(card);
+  } catch (e) {
+    if (e.name === 'ValidationError') {
+      return res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
+    } return res.status(500).send({ message: 'Ошибка по умолчанию.' });
+  }
+};
+
+module.exports = {
+  getCards,
+  createCard,
+  deleteCard,
+  likeCard,
+  dislikeCard,
+};
---
+++ controllers/users.js
@@ -0,0 +1,74 @@
+const User = require('../models/user');
+
+const getUsers = async (req, res) => {
+  try {
+    const users = await User.find({});
+    res.send(200).send(users);
+  } catch (e) {
+    res.status(500).send({ message: 'Ошибка по умолчанию.' });
+  }
+};
+
+const getUserById = async (req, res) => {
+  try {
+    const user = await User.findById(req.params.id);
+    if (!user) {
+      return res.status(404).send({ message: 'Пользователь по указанному _id не найден.' });
+    }
+    return res.send(200).send(user);
+  } catch (e) {
+    return res.status(500).send({ message: 'Ошибка по умолчанию.' });
+  }
+};
+
+const createUser = async (req, res) => {
+  try {
+    const { name, about, avatar } = req.body;
+    const user = await User.create({ name, about, avatar });
+    res.status(201).send(user);
+  } catch (e) {
+    if (e.name === 'ValidationError') {
+      res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
+    } else {
+      res.status(500).send({ message: 'Ошибка по умолчанию.' });
+    }
+  }
+};
+
+const updateUserInfo = async (req, res) => {
+  try {
+    const { name, about } = req.body;
+    const user = await User.findByIdAndUpdate(req.user._id, { name, about }, { new: true });
+    if (!user) {
+      return res.status(404).send({ message: 'Пользователь по указанному _id не найден.' });
+    }
+    return res.status(201).send(user);
+  } catch (e) {
+    if (e.name === 'ValidationError') {
+      return res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
+    } return res.status(500).send({ message: 'Ошибка по умолчанию.' });
+  }
+};
+
+const updateUserAvatar = async (req, res) => {
+  try {
+    const { avatar } = req.body;
+    const user = await User.findByIdAndUpdate(req.user._id, { avatar }, { new: true });
+    if (!user) {
+      return res.status(404).send({ message: 'Пользователь по указанному _id не найден.' });
+    }
+    return res.status(201).send(user);
+  } catch (e) {
+    if (e.name === 'ValidationError') {
+      return res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя.' });
+    } return res.status(500).send({ message: 'Ошибка по умолчанию.' });
+  }
+};
+
+module.exports = {
+  getUsers,
+  getUserById,
+  createUser,
+  updateUserInfo,
+  updateUserAvatar,
+};
